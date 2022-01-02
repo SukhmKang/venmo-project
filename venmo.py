5f04bbd43f5199346a9d5011d7e5df4ad6916217
SukhmKang <86020052+SukhmKang@users.noreply.github.com>
2022-01-02 12:41:18 -0600

    Add files via upload

--- venmo.py
+++ venmo.py
@@ -1,6 +1,6 @@
 import sqlite3
 import sys
-from datetime import datetime
+from helpers import pay, request, unrequest, deposit, transfer, friend, adduser, globallog, friendlog, personallog, transactionslog, requestlog, viewprofile
 
 tags = ["food", "groceries", "rent", "utilities", "sports", "fun", "transportation", "drinks", "business", "tickets", "gift", "gas"]
 db = sqlite3.connect('venmo.db')
@@ -8,114 +8,59 @@ cursor = db.cursor()
 cursor.execute('''CREATE TABLE IF NOT EXISTS paymentLog (senderID TEXT, recipientID TEXT, amount FLOAT, status TEXT, date DATETIME, message TEXT, paymentID TEXT, privacy TEXT, tag TEXT, senderBalance FLOAT, recipientBalance FLOAT)''')
 cursor.execute('''CREATE TABLE IF NOT EXISTS users (username TEXT, friends TEXT, balance FLOAT DEFAULT 0.0, accounttype TEXT, bank TEXT DEFAULT None, privacy TEXT DEFAULT None)''')
 
-if len(sys.argv) == 1:
-    print("Please enter a command.")
-else:
-    if sys.argv[1] == "pay":
-        if len(sys.argv) == 6:
-            pay(sys.argv[2],sys.argv[3],sys.argv[4],sys.argv[5])
-        elif len(sys.argv) == 7:
-            pay(sys.argv[2],sys.argv[3],sys.argv[4],sys.argv[5],sys.argv[6])
-        elif len(sys.argv) == 8:
-            pay(sys.argv[2],sys.argv[3],sys.argv[4],sys.argv[5],sys.argv[6],sys.argv[7])
-    if sys.argv[1] == "request":
-        request()
-    if sys.argv[1] == "unrequest":
-        unrequest()
-    if sys.argv[1] == "deposit":
-        deposit()
-    if sys.argv[1] == "transfer":
-        transfer()
-    if sys.argv[1] == "friend":
-        friend()
-    if sys.argv[1] == "adduser":
-        adduser()
-    if sys.argv[1] == "globallog":
-        globallog()
-    if sys.argv[1] == "friendlog":
-        friendlog()
-    if sys.argv[1] == "personallog":
-        personallog()
-    if sys.argv[1] == "transactionslog":
-        transactionslog()
-    if sys.argv[1] == "requestlog":
-        requestlog()
-    if sys.argv[1] == "viewprofile":
-        viewprofile()
+def init(argv,cursor):
+    if len(argv) == 1:
+        print("Please enter a command.")
+    else:
+        if argv[1] == "pay":
+            if len(argv) == 6:
+                pay(argv[2],argv[3],argv[4],argv[5])
+            elif len(argv) == 7:
+                pay(argv[2],argv[3],argv[4],argv[5],argv[6])
+            elif len(argv) == 8:
+                pay(argv[2],argv[3],argv[4],argv[5],argv[6],argv[7])
+            return
+        if argv[1] == "request":
+            request()
+            return
+        if argv[1] == "unrequest":
+            unrequest()
+            return
+        if argv[1] == "deposit":
+            deposit()
+            return
+        if argv[1] == "transfer":
+            transfer()
+            return
+        if argv[1] == "friend":
+            if len(argv) == 4:
+                friend(argv[2], argv[3], cursor)
+            return
+        if argv[1] == "adduser":
+            if len(argv) == 4:
+                adduser(argv[2], argv[3],cursor)
+            return
+        if argv[1] == "globallog":
+            globallog()
+            return
+        if argv[1] == "friendlog":
+            friendlog()
+            return
+        if argv[1] == "personallog":
+            personallog()
+            return
+        if argv[1] == "transactionslog":
+            transactionslog()
+            return
+        if argv[1] == "requestlog":
+            requestlog()
+            return
+        if argv[1] == "viewprofile":
+            viewprofile()
+            return
+        print("Please enter a valid command.")
 
-
+init(sys.argv,cursor)
 
 db.commit()
 db.close()
-
-def pay(senderID,recipientID,amount,message,tag=None,privacy=None):
-    cursor.execute(''' SELECT username FROM users WHERE username=?''', (senderID))
-    if cursor.fetchone() == None:
-        print("Error: Sender not in system.")
-        return
-    cursor.execute(''' SELECT username FROM users WHERE username =?''', (recipientID))
-    if cursor.fetchone() == None:
-        print("Error: Recipient not in system.")
-        return
-    #error checking: need to check if amount is a numerical value or not
-    #not much to check for the message
-    senderfriends = str(cursor.execute(''' SELECT friends FROM users WHERE username=?''',(senderID)))
-    if len(senderfriends) == 0:
-        print("Error: Recipient is not in sender's friends list.")
-        return
-    elif senderID not in senderfriends.split(","):
-        print("Error: Recipient is not in sender's friends list.")    
-        return
-
-    senderprivacy = cursor.execute(''' SELECT privacy FROM users WHERE username=? ''', (senderID))
-    recipientprivacy = cursor.execute(''' SELECT privacy FROM users WHERE username=? ''', (recipientID))
-
-    if privacy == None: 
-        if senderprivacy or recipientprivacy == "Private":
-            privacy = "Private"
-        elif senderprivacy or recipientprivacy == "Friends Only":
-            privacy = "Friends Only"
-        elif senderprivacy == "Public" and recipientprivacy == "Public":
-            privacy = "Public"
-        else:
-            print("Error: Neither user has a default privacy setting and a privacy setting was not manually inputted.")
-            return
-
-    if privacy != "Private" and privacy != "Friends Only" and privacy != "Public":
-        print("Error: Privacy input must be 'Privacy' or 'Friends Only' or 'Public'.")
-        return
-
-    if tag != None and tag.lower().strip() not in tags:
-        print("Error: Invalid tag.")
-        return
-
-    if float(amount) > float(cursor.execute(''' SELECT balance FROM users WHERE username=? ''', (senderID))):
-        print("Error: Sender does not have enough money in account.")
-        return
-
-    #need to develop hashing function to generate payment ID
-    paymentID = hash(str(senderID)+str(recipientID)+str(message)+str(datetime.now()))
-    senderBalance = float(cursor.execute(''' SELECT balance FROM users WHERE username=? ''', (senderID))) - float(amount)
-    recipientBalance = float(cursor.execute(''' SELECT balance FROM users WHERE username=? ''', (recipientID))) - float(amount)
-    cursor.execute(''' UPDATE users SET balance = ? WHERE username=? ''',(senderBalance,senderID))
-    cursor.execute(''' UPDATE users SET balance = ? WHERE username=? ''',(recipientBalance,recipientID))
-    cursor.execute(''' INSERT INTO paymentLog (senderID, recipientID, amount, status, date, message, paymentID, privacy, tag, senderBalance, recipientBalance)
-    VALUES (?,?,?,?,?,?,?,?,?,?,?) '''
-    (senderID,recipientID,amount,"_payment",datetime.now(),message,paymentID,privacy,tag,senderBalance,recipientBalance))
-    return
-
-def request(userID, friendID,amount,message,tag=None):
-
-def unrequest(paymentID):
-
-def deposit(userID, amount):
-def transfer(userID, amount):
-def friend(userID, friendID):
-def adduser(userID):
-def globallog(argv):
-def friendlog(argv):
-def personallog(argv):
-def transactionslog(argv):
-def requestlog(argv):
-def viewprofile(userID):
-        
