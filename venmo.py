b669e54b8a000755b3f614bf0dfa47c3bbbf4448
SukhmKang <86020052+SukhmKang@users.noreply.github.com>
2022-01-01 20:29:05 -0600

    Add files via upload

--- venmo.py
+++ venmo.py
@@ -1,11 +1,12 @@
 import sqlite3
 import sys
+from datetime import datetime
 
 tags = ["food", "groceries", "rent", "utilities", "sports", "fun", "transportation", "drinks", "business", "tickets", "gift", "gas"]
 db = sqlite3.connect('venmo.db')
 cursor = db.cursor()
 cursor.execute('''CREATE TABLE IF NOT EXISTS paymentLog (senderID TEXT, recipientID TEXT, amount FLOAT, status TEXT, date DATETIME, message TEXT, paymentID TEXT, privacy TEXT, tag TEXT, senderBalance FLOAT, recipientBalance FLOAT)''')
-cursor.execute('''CREATE TABLE IF NOT EXISTS users (username TEXT, friends TEXT, balance FLOAT, accounttype TEXT)''')
+cursor.execute('''CREATE TABLE IF NOT EXISTS users (username TEXT, friends TEXT, balance FLOAT DEFAULT 0.0, accounttype TEXT, bank TEXT DEFAULT None, privacy TEXT DEFAULT None)''')
 
 if len(sys.argv) == 1:
     print("Please enter a command.")
@@ -15,6 +16,8 @@ else:
             pay(sys.argv[2],sys.argv[3],sys.argv[4],sys.argv[5])
         elif len(sys.argv) == 7:
             pay(sys.argv[2],sys.argv[3],sys.argv[4],sys.argv[5],sys.argv[6])
+        elif len(sys.argv) == 8:
+            pay(sys.argv[2],sys.argv[3],sys.argv[4],sys.argv[5],sys.argv[6],sys.argv[7])
     if sys.argv[1] == "request":
         request()
     if sys.argv[1] == "unrequest":
@@ -45,9 +48,44 @@ else:
 db.commit()
 db.close()
 
-def pay(senderID,recipientID,amount,message,tag=None):
+def pay(senderID,recipientID,amount,message,tag=None,privacy=None):
+    cursor.execute(''' SELECT username FROM users WHERE username=?''', (senderID))
+    if cursor.fetchone() == None:
+        print("Error: Sender not in system.")
+        return
+    cursor.execute(''' SELECT username FROM users WHERE username =?''', (recipientID))
+    if cursor.fetchone() == None:
+        print("Error: Recipient not in system.")
+        return
+    #error checking: need to check if amount is a numerical value or not
+    #not much to check for the message
+    senderprivacy = cursor.execute(''' SELECT privacy FROM users WHERE username=? ''', (senderID))
+    recipientprivacy = cursor.execute(''' SELECT privacy FROM users WHERE username=? ''', (recipientID))
+    if senderprivacy or recipientprivacy == "Private":
+        privacy = "Private"
+    elif senderprivacy or recipientprivacy == "Friends Only":
+        privacy = "Friends Only"
+    elif senderprivacy == "Public" and recipientprivacy == "Public":
+        privacy = "Public"
+    else:
+        print("Error: Neither user has a default privacy setting and a privacy setting was not manually inputted.")
+        return
+    
+    if tag != None and tag.lower().strip() not in tags:
+        print("Error: Invalid tag.")
+        return
 
+    if float(amount) > float(cursor.execute(''' SELECT balance FROM users WHERE username=? ''', (senderID))):
+        print("Error: Sender does not have enough money in account.")
+        return
 
+    #need to develop hashing function to generate payment ID
+    paymentID = hash(str(senderID)+str(recipientID)+str(message)+str(datetime.now()))
+    senderBalance = float(cursor.execute(''' SELECT balance FROM users WHERE username=? ''', (senderID))) - float(amount)
+    recipientBalance = float(cursor.execute(''' SELECT balance FROM users WHERE username=? ''', (recipientID))) - float(amount)
+    cursor.execute(''' INSERT INTO paymentLog (senderID, recipientID, amount, status, date, message, paymentID, privacy, tag, senderBalance, recipientBalance)
+    VALUES (?,?,?,?,?,?,?,?,?,?,?) '''
+    (senderID,recipientID,amount,"_payment",datetime.now(),message,paymentID,privacy,tag,senderBalance,recipientBalance))
 def request(userID, friendID,amount,message,tag=None):
 
 def unrequest(paymentID):
